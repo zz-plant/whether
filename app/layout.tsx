3c72540fb95871256d3ac2d0cb8638a0d72d2b59
zz plant <kanavj@gmail.com>
2026-02-09 16:04:31 -0600

    Restore mobile zoom support in viewport config

--- app/layout.tsx
+++ app/layout.tsx
@@ -58,9 +58,6 @@ export const metadata: Metadata = {
 export const viewport: Viewport = {
   width: "device-width",
   initialScale: 1,
-  minimumScale: 1,
-  maximumScale: 1,
-  userScalable: false,
   themeColor: "#020617",
 };
 
