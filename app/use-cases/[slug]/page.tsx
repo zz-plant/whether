7188de414f21cc081b842792fe1f910e2fb9b0c7
Kanav Jain <kanavj@gmail.com>
2026-02-27 17:38:03 -0600

    Mark use-cases slug redirect route as edge runtime

--- app/use-cases/[slug]/page.tsx
+++ app/use-cases/[slug]/page.tsx
@@ -2,6 +2,8 @@ import { redirectWithParams } from "../../../lib/navigation/legacyRedirects";
 
 type Params = { slug: string };
 
+export const runtime = "edge";
+
 export default function UseCaseDetailPage({ params }: { params: Promise<Params> }) {
   return redirectWithParams(params, ({ slug }) => `/decide/${slug}`);
 }
